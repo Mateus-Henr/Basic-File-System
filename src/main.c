24de7ad7f36870ac2ac9627ba5a053d54a6d0131
AlanGabriews <ms.alangabriel@gmail.com>
2023-07-08 00:38:20 -0300

    entrada por arquivo feita

--- src/main.c
+++ src/main.c
@@ -13,6 +13,78 @@ int main()
     UFS ufs;
     initializeUFS(&ufs, MAX_INODES);
 
+    while(true)
+    {
+        char inputType[UCHAR_MAX];
+
+        printf("Would you like to enter a command file? (Y/n)?\n");
+
+        if(fgets(inputType, sizeof(inputType), stdin) != NULL)
+        {
+            if(inputType[0] == '\n')
+            {
+                continue;
+            }
+
+            if((inputType[0] == 'Y' || inputType[0] == 'y') && inputType[1] == '\n')
+            {
+                char fileName[UCHAR_MAX - 6];
+
+                while(true)
+                {
+                    printf("Enter the file name: ");
+
+                    if(fgets(fileName, sizeof(fileName), stdin) != NULL)
+                    {
+                        fileName[strcspn(fileName, "\n")] = '\0';
+
+                        char filePath[UCHAR_MAX];
+                        snprintf(filePath, sizeof(filePath), "files/%s", fileName);
+
+                        FILE* file = fopen(filePath, "r");
+            
+                        if(file != NULL)
+                        {    
+                            char commandString[UCHAR_MAX];
+
+                            while (fgets(commandString, sizeof(commandString), file))
+                            {
+                                commandString[strcspn(commandString, "\n")] = '\0';
+                                commandString[strcspn(commandString, "\r")] = '\0';
+
+                                Command* command = initializeCommand(commandString);
+
+                                if (!command)
+                                {
+                                    continue;
+                                }
+
+                                commandSwitch(&ufs, command);
+                                freeCommand(command);
+                            }
+
+                            fclose(file);
+
+                            return 0;
+                        }
+                        else
+                        {
+                            printf(FILE_ERROR, fileName);
+                        }
+                    }
+                    else
+                    {            
+                        printf(INPUT_ERROR);
+                    }
+                }
+            }
+            else break;
+        }
+        else
+        {
+            printf(INPUT_ERROR);
+        }
+    }
     printf(VALID_COMMANDS);
 
     while (true)
